88791eaef86ccf5e8832c453ab0ac1a16771a257
KaiselDu7 <eduardo.gomes29@escola.pr.gov.br>
2025-09-03 12:26:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Jogos',
+    'Qual é o jogo mais vendido de todos os tempos?',
+    'O jogo mais vendido de todos os tempos é "Minecraft'
+)
+
+criaCartao(
+    'Carro',
+    'Qual foi o primeiro carro produzido em massa?',
+    'O primeiro carro produzido em massa foi o Ford Modelo T'
+)
+
+criaCartao(
+    'Esporte',
+    'O que é o curling?',
+    'O curling é um esporte no qual os jogadores deslizam pedras pesadas sobre o gelo em direção a uma área alvo'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual filme ganhou o Oscar de Melhor Filme em 2020?',
+    'O filme que ganhou o Oscar de Melhor Filme em 2020 foi "Parasita"'
+)
